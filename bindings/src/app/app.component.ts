0b60b02b40c1ecc85bafb57433a8ad20e2dde027
Yehia Saleh <yehia.saleh@yale.edu>
2017-02-05 15:35:29 -0500

    Class and style binding

--- bindings/src/app/app.component.ts
+++ bindings/src/app/app.component.ts
@@ -12,10 +12,26 @@ import { Component } from '@angular/core';
   <input (keyup)='userInput($event)'>
   <p>{{ myType }}</p>
 
-  `
+  <button class="my-btn" [class.extraclass]="someProperty">Call to Action</button>
+  <button class="my-btn" [style.border]="someProperty ? '5px solid yellow' : 'none'">Call to Action</button>
+  <br><br>
+
+  <button class="my-btn" [ngClass]="setClasses()">Call to Action</button>
+  <button class="my-btn" [ngStyle]="setStyles()">Call to Action</button>
+  `,
+
+
+  styles: [`
+    .my-btn { font-size: 1.7em; }
+    .extraclass { background: black; color: white; }
+    .anotherclass { font-weight: bold; }
+  `]
 })
 export class AppComponent {
   myType = '';
+  someProperty = true;
+  anotherProperty = true;
+
   imageUrl = 'http://img10.deviantart.net/e984/i/2015/287/c/5/red_dragon_by_sandara-d6hpycs.jpg';
 
   myMethod() {
@@ -28,5 +44,21 @@ export class AppComponent {
 
   userInput(event: any) {
     this.myType = event.target.value;
+  }
+
+  setClasses() {
+    let classes = {
+      extraclass: this.someProperty,
+      anotherclass: this.anotherProperty
+    };
+    return classes;
+  }
+
+  setStyles() {
+    let styles = {
+      'font-style': this.someProperty ? 'italic' : '',
+      'font-weight': this.anotherProperty ? 'bold' : ''
+    };
+    return styles;
   }
 }
